72f5d740f52f01b202a556c5907096350e1a8043
Kasia <katarzyna.tramea@gmail.com>
2021-10-17 11:03:35 +0200

    add render(), create() and dispose(), add 2 new fields

--- core/src/com/mygdx/feedyourhead/FeedYourHead.java
+++ core/src/com/mygdx/feedyourhead/FeedYourHead.java
@@ -1,31 +1,27 @@
 package com.mygdx.feedyourhead;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FeedYourHead extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
+
+	public SpriteBatch batch;
+	public BitmapFont font;
+
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("");
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render();
 	}
-	
-	@Override
-	public void dispose () {
+
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
+
 }
