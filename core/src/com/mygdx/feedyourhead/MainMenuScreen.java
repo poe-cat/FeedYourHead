6ae11a9429b557b290bb6bafa18949a0e9b23b8f
Kasia <katarzyna.tramea@gmail.com>
2021-10-17 11:09:06 +0200

    create and add OrthograpficCamera to constructor

--- core/src/com/mygdx/feedyourhead/MainMenuScreen.java
+++ core/src/com/mygdx/feedyourhead/MainMenuScreen.java
@@ -1,13 +1,18 @@
 package com.mygdx.feedyourhead;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class MainMenuScreen implements Screen {
 
     final FeedYourHead game;
+    OrthographicCamera camera;
 
     public MainMenuScreen(final FeedYourHead game) {
         this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
     }
 
 
