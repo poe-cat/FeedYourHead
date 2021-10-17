2d8ebbd0475032bf61971515b889dded93723a27
Kasia <katarzyna.tramea@gmail.com>
2021-10-17 11:04:31 +0200

    set screen as main menu screen (and create proper class)

---
+++ core/src/com/mygdx/feedyourhead/MainMenuScreen.java
@@ -0,0 +1,8 @@
+package com.mygdx.feedyourhead;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen {
+    public MainMenuScreen(FeedYourHead feedYourHead) {
+    }
+}
