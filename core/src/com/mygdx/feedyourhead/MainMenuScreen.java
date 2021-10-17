60a76bd8eca392af24cc268f5ed5e5beccb0ce34
Kasia <katarzyna.tramea@gmail.com>
2021-10-17 11:18:02 +0200

    update render() with batch setting methods

--- core/src/com/mygdx/feedyourhead/MainMenuScreen.java
+++ core/src/com/mygdx/feedyourhead/MainMenuScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.feedyourhead;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainMenuScreen implements Screen {
 
@@ -22,6 +23,15 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "* Come in & Feed Your Head *", 100, 150);
+        game.font.draw(game.batch, "-> Click anywhere to start <-", 100, 100);
+        game.batch.end();
 
     }
 
